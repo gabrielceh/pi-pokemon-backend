1b9bb64fb964c79c8f5c5c1f64827c4040c491a4
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-06-13 11:38:40 -0500

    solucion bug en typeFilterById

--- src/controllers/pokemon/getPokemon.js
+++ src/controllers/pokemon/getPokemon.js
@@ -1,22 +1,22 @@
-const { Op } = require('sequelize');
-const CustomError = require('../../classes/CustomError');
-const { Pokemon_Api, Pokemon } = require('../../db');
-const { getMyHost } = require('../../utils/localhost');
-const { optionsApi, optionsUser, limitToSearchInApi } = require('../../utils/optionToFindPokemon');
-const { pagination } = require('../../utils/pagination');
-const { getPokemonSlice } = require('../../utils/pokemonSlice');
-const { POKE_API_URL, POKEMON_SOURCE } = require('../../utils/pokeApiUrl');
-const { getPokemonData } = require('../../utils/getPokemonData');
-const { orderPokemonList } = require('../../utils/orderPokemonList');
-const { default: axios } = require('axios');
-const { NAME_REGEX } = require('../../utils/regex');
+const { Op } = require('sequelize')
+const CustomError = require('../../classes/CustomError')
+const { Pokemon_Api, Pokemon } = require('../../db')
+const { getMyHost } = require('../../utils/localhost')
+const { optionsApi, optionsUser, limitToSearchInApi } = require('../../utils/optionToFindPokemon')
+const { pagination } = require('../../utils/pagination')
+const { getPokemonSlice } = require('../../utils/pokemonSlice')
+const { POKE_API_URL, POKEMON_SOURCE } = require('../../utils/pokeApiUrl')
+const { getPokemonData } = require('../../utils/getPokemonData')
+const { orderPokemonList } = require('../../utils/orderPokemonList')
+const { default: axios } = require('axios')
+const { NAME_REGEX } = require('../../utils/regex')
 
-let pokemonApiList = [];
+let pokemonApiList = []
 
 const getPokemonByName = async (res, name, optionsUser) => {
 	try {
 		if (!NAME_REGEX.test(name)) {
-			return res.status(400).json({ error: `'${name}' is not a valid name` });
+			return res.status(400).json({ error: `'${name}' is not a valid name` })
 		}
 
 		const pokemonUserFinded = await Pokemon.findOne({
@@ -24,25 +24,25 @@ const getPokemonByName = async (res, name, optionsUser) => {
 				name: name.toLowerCase(),
 			},
 			...optionsUser,
-		});
+		})
 
 		if (pokemonUserFinded) {
-			return res.status(200).json(pokemonUserFinded);
+			return res.status(200).json(pokemonUserFinded)
 		}
 
 		// const dataApi = await axios.get(`${POKE_API_URL}/${POKEMON_SOURCE}/${name.toLowerCase()}`);
-		const dataApi = await getPokemonData(name.toLowerCase());
+		const dataApi = await getPokemonData(name.toLowerCase())
 
 		if (dataApi.status >= 400) {
-			throw new CustomError(400, `Pokemon '${name}' not found`);
+			throw new CustomError(400, `Pokemon '${name}' not found`)
 		}
 
-		return res.status(200).json(dataApi);
+		return res.status(200).json(dataApi)
 	} catch (error) {
-		const status = error.status || 500;
-		res.status(status).json({ error: error.message });
+		const status = error.status || 500
+		res.status(status).json({ error: error.message })
 	}
-};
+}
 
 const getAllPokemon = async (res, req, optionsUser) => {
 	/**Obtine los pokemos originales de la db y los pokemon creados por los usuarios
@@ -51,29 +51,29 @@ const getAllPokemon = async (res, req, optionsUser) => {
 	 * Devuelve los pokemon ordenados por id(defaul), name o attack
 	 */
 	try {
-		let { offset, limit, orderby, ordertype } = req.query;
-		const myHost = getMyHost(req);
+		let { offset, limit, orderby, ordertype } = req.query
+		const myHost = getMyHost(req)
 
 		if (!pokemonApiList.length) {
 			const apiPokemon = await axios.get(
 				`${POKE_API_URL}/${POKEMON_SOURCE}/?offset=0&limit=${limitToSearchInApi}`
-			);
-			const { results } = apiPokemon.data;
+			)
+			const { results } = apiPokemon.data
 
-			pokemonApiList = await Promise.all(results.map((pokemon) => getPokemonData(pokemon.name)));
+			pokemonApiList = await Promise.all(results.map((pokemon) => getPokemonData(pokemon.name)))
 		}
-		const pokemonUserList = await Pokemon.findAll(optionsUser);
+		const pokemonUserList = await Pokemon.findAll(optionsUser)
 
-		let pokemonData = [...pokemonApiList, ...pokemonUserList];
+		let pokemonData = [...pokemonApiList, ...pokemonUserList]
 
 		if (orderby && ordertype) {
-			pokemonData = orderPokemonList([...pokemonApiList, ...pokemonUserList], orderby, ordertype);
+			pokemonData = orderPokemonList([...pokemonApiList, ...pokemonUserList], orderby, ordertype)
 		}
 
-		offset = offset ? +offset : 0;
-		limit = limit ? +limit : 12;
+		offset = offset ? +offset : 0
+		limit = limit ? +limit : 12
 
-		const orderString = orderby && ordertype ? `&orderby=${orderby}&ordertype=${ordertype}` : '';
+		const orderString = orderby && ordertype ? `&orderby=${orderby}&ordertype=${ordertype}` : ''
 		const { count, next, prev, dataList, maxPage, currentPage } = pagination(
 			req,
 			getPokemonSlice,
@@ -82,23 +82,23 @@ const getAllPokemon = async (res, req, optionsUser) => {
 			limit,
 			`pokemon-api/pokemon`,
 			orderString
-		);
+		)
 
-		res.status(200).json({ count, next, prev, results: dataList });
+		res.status(200).json({ count, next, prev, results: dataList })
 	} catch (error) {
-		const status = error.status || 500;
-		res.status(status).json({ error: error.message });
+		const status = error.status || 500
+		res.status(status).json({ error: error.message })
 	}
-};
+}
 
 const getPokemon = (req, res) => {
-	let { name } = req.query;
+	let { name } = req.query
 
 	if (name) {
-		return getPokemonByName(res, name, optionsUser);
+		return getPokemonByName(res, name, optionsUser)
 	} else {
-		return getAllPokemon(res, req, optionsUser);
+		return getAllPokemon(res, req, optionsUser)
 	}
-};
+}
 
-module.exports = { getPokemon };
+module.exports = { getPokemon }
--- src/controllers/types.controller.js
+++ src/controllers/types.controller.js
@@ -1,28 +1,28 @@
-const CustomError = require('../classes/CustomError');
-const { Types, Pokemon, Pokemon_Api, User } = require('../db');
-const { getTypesFromApi } = require('../utils/getTypesFromApi');
-const { pagination } = require('../utils/pagination');
-const { POKE_API_URL, TYPE_SOURCE, POKEMON_SOURCE } = require('../utils/pokeApiUrl');
-const { getMyHost } = require('../utils/localhost');
-const { orderBy, orderTypes } = require('../utils/orderValues');
-const { orderPokemonList } = require('../utils/orderPokemonList');
+const CustomError = require('../classes/CustomError')
+const { Types, Pokemon, Pokemon_Api, User } = require('../db')
+const { getTypesFromApi } = require('../utils/getTypesFromApi')
+const { pagination } = require('../utils/pagination')
+const { POKE_API_URL, TYPE_SOURCE, POKEMON_SOURCE } = require('../utils/pokeApiUrl')
+const { getMyHost } = require('../utils/localhost')
+const { orderBy, orderTypes } = require('../utils/orderValues')
+const { orderPokemonList } = require('../utils/orderPokemonList')
 const {
 	attributes,
 	includeTypes,
 	optionsApi,
 	optionsUser,
 	limitToSearchInApi,
-} = require('../utils/optionToFindPokemon');
-const { getPokemonData } = require('../utils/getPokemonData');
-const { default: axios } = require('axios');
+} = require('../utils/optionToFindPokemon')
+const { getPokemonData } = require('../utils/getPokemonData')
+const { default: axios } = require('axios')
 
-let pokemonApiList = [];
+let pokemonApiList = []
 
 /**Funcion para el paginado de los pokemon de la base de datos */
 const filterByIdSlice = (data = [], offset = 0, limit = 12) => {
-	const dataSlice = data.slice(offset, offset + limit);
-	return dataSlice;
-};
+	const dataSlice = data.slice(offset, offset + limit)
+	return dataSlice
+}
 
 /** Obtenemos los pokemos filtrados por el id del tipo y nos devolverá solo el id del los que
  * cumplen con la condicion, y si vienen los parametros orderby y order los ordenará.
@@ -36,7 +36,7 @@ const getPokemonByTypesDBList = async (
 	ordertype = null,
 	options = {}
 ) => {
-	const order = orderby && ordertype && [[orderby, ordertype]];
+	const order = orderby && ordertype && [[orderby, ordertype]]
 
 	const pokemonByType = await model.findAll({
 		order,
@@ -51,102 +51,102 @@ const getPokemonByTypesDBList = async (
 				where: { id: id },
 			},
 		],
-	});
+	})
 
-	let pokemonWithType = [];
+	let pokemonWithType = []
 
 	for (let pok of pokemonByType) {
 		const pk = await model.findOne({
 			where: { id: pok.id },
 			...options,
-		});
-		pokemonWithType.push(pk);
+		})
+		pokemonWithType.push(pk)
 	}
 
-	return pokemonWithType;
-};
+	return pokemonWithType
+}
 
 const getPokemonByTypesAPIList = (typeId, pokemonList = [], orderby, ordertype) => {
-	let filteredData = [];
+	let filteredData = []
 
 	for (let pokemon of pokemonList) {
 		for (let type of pokemon.Types) {
 			if (type.id === typeId) {
-				filteredData.push(pokemon);
+				filteredData.push(pokemon)
 			}
 		}
 	}
 
-	filteredData = orderPokemonList(filteredData, orderby, ordertype);
+	filteredData = orderPokemonList(filteredData, orderby, ordertype)
 
-	return filteredData;
-};
+	return filteredData
+}
 
 /**Funcion que nos permitira obtener los types de la api y agregarlos a la base de datos
  * Retornará un arreglo de types
  */
 const getTypes = async (req, res) => {
 	try {
-		const allTypes = await Types.findAll();
-		let typeToReturn = [];
+		const allTypes = await Types.findAll()
+		let typeToReturn = []
 
 		if (!allTypes.length) {
-			const typesFromApi = await getTypesFromApi();
+			const typesFromApi = await getTypesFromApi()
 
 			if (typesFromApi.status >= 400) {
-				throw new CustomError(response.status, response.statusText);
+				throw new CustomError(response.status, response.statusText)
 			}
 
-			const newTypes = await Types.bulkCreate(typesFromApi);
+			const newTypes = await Types.bulkCreate(typesFromApi)
 			typeToReturn = newTypes.map((type) => {
 				return {
 					id: type.dataValues.id,
 					name: type.dataValues.name,
-				};
-			});
-			return res.status(201).json({ results: typeToReturn });
+				}
+			})
+			return res.status(201).json({ results: typeToReturn })
 		}
 
 		typeToReturn = allTypes.map((type) => {
 			return {
 				id: type.dataValues.id,
 				name: type.dataValues.name,
-			};
-		});
+			}
+		})
 
-		res.status(200).json({ results: typeToReturn });
+		res.status(200).json({ results: typeToReturn })
 	} catch (error) {
-		const status = error.status || 500;
-		res.status(status).json({ error: error.message });
+		const status = error.status || 500
+		res.status(status).json({ error: error.message })
 	}
-};
+}
 
 const typeFilterById = async (req, res) => {
 	try {
-		const { id } = req.params;
-		let { offset, limit, orderby, ordertype } = req.query;
-		const myHost = getMyHost(req);
+		const { id } = req.params
+		let { offset, limit, orderby, ordertype } = req.query
+		const myHost = getMyHost(req)
 
 		if (isNaN(id)) {
-			throw new CustomError(404, 'Please, send a numeric id');
+			throw new CustomError(404, 'Please, send a numeric id')
 		}
 
-		const foundedType = await Types.findByPk(+id);
+		const foundedType = await Types.findByPk(+id)
 
 		if (!foundedType) {
-			throw new CustomError(404, 'The type id is not in the data base');
+			throw new CustomError(404, 'The type id is not in the data base')
 		}
 
-		offset = offset ? +offset : 0;
-		limit = limit ? +limit : 12;
+		offset = offset ? +offset : 0
+		limit = limit ? +limit : 12
 
 		if (!pokemonApiList.length) {
 			const apiPokemon = await axios.get(
 				`${POKE_API_URL}/${POKEMON_SOURCE}/?offset=0&limit=${limitToSearchInApi}`
-			);
-			const { results } = apiPokemon.data;
+			)
+			const { results } = apiPokemon.data
 
-			pokemonApiList = await Promise.all(results.map((pokemon) => getPokemonData(pokemon.name)));
+			pokemonApiList = await Promise.all(results.map((pokemon) => getPokemonData(pokemon.name)))
 		}
 
 		const getPokemonUserByType = await getPokemonByTypesDBList(
@@ -155,38 +155,38 @@ const typeFilterById = async (req, res) => {
 			orderby,
 			ordertype,
 			optionsUser
-		);
+		)
 
-		const getPokemonApiByType = getPokemonByTypesAPIList(+id, pokemonApiList, orderby, ordertype);
+		const getPokemonApiByType = getPokemonByTypesAPIList(+id, pokemonApiList, orderby, ordertype)
 
-		let listPokemonOrdered = [...getPokemonApiByType, ...getPokemonUserByType];
+		let listPokemonOrdered = [...getPokemonApiByType, ...getPokemonUserByType]
 
 		if (ordertype && orderby) {
-			listPokemonOrdered = orderPokemonList(listPokemonOrdered, orderby, ordertype);
+			listPokemonOrdered = orderPokemonList(listPokemonOrdered, orderby, ordertype)
 		}
 
-		const orderString = orderby && ordertype ? `&orderby=${orderby}&ordertype=${ordertype}` : '';
+		const orderString = orderby && ordertype ? `&orderby=${orderby}&ordertype=${ordertype}` : ''
 		const { count, next, prev, dataList, maxPage, currentPage } = pagination(
 			req,
 			filterByIdSlice,
 			listPokemonOrdered,
 			offset,
 			limit,
-			`types/${id}`,
+			`pokemon-api/types/${id}`,
 			orderString
-		);
+		)
 
 		res
 			.status(200)
-			.json({ count, next, prev, name: foundedType.dataValues.name, results: dataList });
+			.json({ count, next, prev, name: foundedType.dataValues.name, results: dataList })
 	} catch (error) {
-		const status = error.status || 500;
-		res.status(status).json({ error: error.message });
+		const status = error.status || 500
+		res.status(status).json({ error: error.message })
 	}
-};
+}
 
 module.exports = {
 	getTypes,
 	// filterTypesApi,
 	typeFilterById,
-};
+}
